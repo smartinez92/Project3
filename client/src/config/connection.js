fcfe7c0b17549380216ff1fe1a62952bc66f8d93
Steven Abz Martinez <s.martinez92@live.com>
2018-10-27 04:50:05 -0400

    PLEASEFORTHELOVEOFGOD

--- client/src/config/connection.js
+++ client/src/config/connection.js
@@ -1,6 +1,6 @@
 import mongoose from 'mongoose';
 
-mongoose.connect("mongodb://heroku_l3g0:a1234567890@ds143573.mlab.com:43573/heroku_sc3kkc3w", function(err) {
+mongoose.connect("mongodb://<heroku_l3g0>:<a1234567890>@ds143573.mlab.com:43573/heroku_sc3kkc3w", function(err) {
 	if(err) throw err;
 	console.log('database connected');
 });
