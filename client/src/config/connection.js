386a97585ac8064b8cc8681d9977157910f8e717
Steven Abz Martinez <s.martinez92@live.com>
2018-10-27 05:06:08 -0400

    igveup

--- client/src/config/connection.js
+++ client/src/config/connection.js
@@ -1,6 +1,6 @@
 import mongoose from 'mongoose';
 
-mongoose.connect("mongodb://<heroku_l3g0>:<a1234567890>@ds143573.mlab.com:43573/heroku_sc3kkc3w", function(err) {
+mongoose.connect("mongodb://abz:a123456789@cluster0-shard-00-00-hcxfs.mongodb.net:27017,cluster0-shard-00-01-hcxfs.mongodb.net:27017,cluster0-shard-00-02-hcxfs.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true", function(err) {
 	if(err) throw err;
 	console.log('database connected');
 });
